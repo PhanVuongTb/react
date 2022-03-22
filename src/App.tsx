040a7de8868897a41df7c46fcc41eee96248001e
Phan Vuong <pdvuongtb@gmail.com>
2022-03-22 16:33:57 +0700

    buổi 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,59 @@
-// import { useState } from 'react'
-// import logo from './logo.svg'
-// import './App.css'
-// import ShowInfo from './components/ShowInfo'
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [myName, setMyName] = useState("Le Trong Dat");
-//   const [status, setStatus] = useState(false);
-//   const [info, setInfo] = useState({ name: "Dat", age: 20});
-//   const [products, setProducts] = useState([
-//     {id: 1, name: "Product A"},
-//     {id: 2, name: "Product B"},
-//     {id: 3, name: "Product C"}
-//   ])
-//   return (
-//     <div className="App">
-//       Count: {count} 
-//       <hr />
-//       Full Name: {myName}
-//       <hr />
-//       Status: {status ? "True" : "False"}
-//       <hr />
-//       Info: {info.name} - {info.age}
-//       <hr />
-//       Products: { products.map(item => <div>{item.name}</div>)}
-//       <hr />
-//       Component: ShowInfo
-//       <ShowInfo name="Dat" age={20}/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// // function useState(state){
-// //   return [state, function(){}];
-// // }
-
-// // const [firstValue, secondValue] = useState(10);
-// // console.log(firstValue) // 10
-
-// // secondValue(20); //
-
-// // console.log(firstValue) // 20
-
-
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { NavLink, Route, Routes } from 'react-router-dom'
+import { Navigate, NavLink, Route, Router, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
-import ProductPage from './pages/Product'
-import Aboutpage from './pages/Aboutpage'
+import WebsiteLayout from './pages/layouts/WebsiteLayout'
+import AdminLayout from './pages/layouts/AdminLayout'
+import ProductDetail from './pages/ProductDetail'
+import ProductManager from './pages/ProductManager';
+import { ProductType } from './pages/types/product'
+import { list, remove } from './api/product'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState(false);
+
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+      const getProducts = async () => {
+            const { data } = await list();
+            setProducts(data);
+      }
+      getProducts();
+  }, [])
+
+  const removeItem = (id) => {
+    
+    remove(id);
+    // reRender
+    setProducts(products.filter(item => item.id !== id));
+
+
+    // setProduct()
+  }
+  
   return (
+
     <div className="container">
-      <header>
-        <ul>
-          <li><NavLink to="/">Home Page</NavLink></li>
-          <li><NavLink to="/product">Product</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-        </ul>
-      </header>
-      <main>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="product" element={<ProductPage />}/>
-            <Route path="about" element={<Aboutpage />}/>
-          </Routes>
-      </main>
+      {count} <button onClick={() => setCount(count + 1)}>Click</button>
+      <button onClick={() => setStatus(true)}>Click</button>
+        <Routes>
+          <Route path="/" element={<WebsiteLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="products">
+                <Route index element={<h1>Product Page</h1>} />
+                <Route path=":id" element={<ProductDetail />} />
+              </Route>
+              
+          </Route>
+          <Route path="admin" element={<AdminLayout />}>
+              <Route index element={<Navigate to="dashboard" />} />
+              <Route path="dashboard" element={<h1>Dashboard page</h1>} />
+              <Route path="products" element={<ProductManager products={products} onRemove={removeItem}/>} />
+          </Route>
+        </Routes>
     </div>
   )
 }
